e795dad7fb02a63418564e3c283b63d1cf3f4ef3
sk1nhead <zamaulana22@gmail.com>
2022-01-22 11:22:29 +0700

    Fitur Pertama  -Menambahkan Expanded  -Menambahkan Container

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const MyHomePage(title: 'Flutter Latihan'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [ IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          children: [
+            Expanded(child: )
+          ],
+
+        ),
+      ),
+    );
+  }
+}
